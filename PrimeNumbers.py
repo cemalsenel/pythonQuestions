a3ff3ab310f70ba3770cb9836c50ae7967ff9c25
cemalsenel <senelcemal61@gmail.com>
2021-05-03 13:57:49 +0200

    updated prime numbers

--- PrimeNumbers.py
+++ PrimeNumbers.py
@@ -1,17 +1,16 @@
 number = input("Enter a number")
-
 while number.isnumeric==False or int(number) <= 2:
     print("Please enter a valid number")
     number = input("Enter a number")
 
-check = True
+sum = 0
 prime=[]
 for i in list(range(2,int(number)+1)):
     for j in list(range(2, int(i))):
           if i % j == 0:
-            check = False
-    if check == False:
-        check = True
+            sum +=1
+    if sum > 0:
+        sum = 0
     else:
         prime.append(i)    
 print(prime)
